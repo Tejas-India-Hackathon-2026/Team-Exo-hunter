92e5626a4ba22cd37d2af82b0795ce500b5366c8
Ayush Raj <alok.raj443@gmail.com>
2026-08-20 16:40:49 +0530

    config: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Team-Exo-hunter/',
   plugins: [
     react(),
     tailwindcss(),
